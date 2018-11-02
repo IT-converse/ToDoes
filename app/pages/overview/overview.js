0f9710ecb636e58126a32808bdce2f6d71df4db7
sybren marechal <sybrenmarechal@icloud.com>
2018-11-03 00:41:17 +0100

    add nico overvieuw

---
+++ app/pages/overview/overview.js
@@ -0,0 +1,33 @@
+var Observable = require("data/observable");
+const httpModule = require("http");
+const frameModule = require("ui/frame");
+
+// We can use auth-tokens via header
+
+exports.onLoaded = function(args) {
+  let page = args.object;
+  let projects = new Observable.fromObject({
+    projects: []
+  });
+  page.bindingContext = projects;
+
+  httpModule
+    .request({
+      url: "http://192.168.1.105:3000/projects.json",
+      method: "GET",
+      headers: { "Content-Type": "application/json" }
+    })
+    .then(
+      response => {
+        console.log(response.statusCode);
+        if (response.statusCode == 200) {
+          projects.projects = response.content.toJSON();
+        } else {
+          Toast.makeText("Failed to fetch projects", "long").show();
+        }
+      },
+      e => {
+        Toast.makeText("Failed to get projects", "long").show();
+      }
+    );
+};
