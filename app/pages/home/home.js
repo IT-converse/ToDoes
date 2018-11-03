1125676c388615b725bf9a394d8788b5a9377d58
sybren marechal <sybrenmarechal@icloud.com>
2018-11-03 01:31:46 +0100

    add tasks on home page

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -1,5 +1,7 @@
 var view = require("ui/core/view");
 const frameModule = require("ui/frame");
+var Observable = require("data/observable");
+const httpModule = require("http");
 
 var drawer;
 
@@ -30,4 +32,34 @@ exports.onOverview = function() {
 
 exports.onLogOUt = function() {
   traverse("login");
+};
+
+// We can use auth-tokens via header
+
+exports.onLoaded = function(args) {
+  let page = args.object;
+  let projects = new Observable.fromObject({
+    projects: []
+  });
+  page.bindingContext = projects;
+
+  httpModule
+    .request({
+      url: "http://192.168.1.105:3000/projects.json",
+      method: "GET",
+      headers: { "Content-Type": "application/json" }
+    })
+    .then(
+      response => {
+        console.log(response.statusCode);
+        if (response.statusCode == 200) {
+          projects.projects = response.content.toJSON();
+        } else {
+          Toast.makeText("Failed to fetch projects", "long").show();
+        }
+      },
+      e => {
+        Toast.makeText("Failed to get projects", "long").show();
+      }
+    );
 };
