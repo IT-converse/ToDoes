f4f83a563589720c7733bc8b30a7c6506e6759e8
sybren marechal <sybrenmarechal@icloud.com>
2018-11-10 05:40:28 +0100

    eff

---
+++ app/pages/route/route.js
@@ -0,0 +1,17 @@
+const frameModule = require("ui/frame");
+
+
+function traverse(page) {
+    console.log("traversing to " + page);
+    frameModule.topmost().navigate({
+      moduleName: "pages/" + page + "/" + page,
+      transition: "slideBottom"
+    });
+  }
+  
+  exports.onLogin = function() {
+    traverse("home");
+  };
+  exports.onSybren = function() {
+    traverse("map");
+  };
